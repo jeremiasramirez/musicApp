d20012fb2064cef488d3c99bf01830d8d1ea7cfa
Jeremias ramirez <jereramirez499@gmail.com>
2020-04-17 00:28:23 -0400

    agregando cambiadore de rutas en componente search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatButtonModule } from "@angular/material/button";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import {MatRippleModule} from '@angular/material/core';
-
+import {MatButtonToggleModule} from '@angular/material/button-toggle'
 @NgModule({
   declarations: [
 
@@ -41,7 +41,8 @@ import {MatRippleModule} from '@angular/material/core';
     BrowserAnimationsModule,
     MatRippleModule,
     MatButtonModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
